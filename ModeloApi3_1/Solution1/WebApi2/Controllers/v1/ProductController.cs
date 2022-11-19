09ad9ec91221faa1169176c616df8d3417010571
tdr <tdr.evolution@gmail.com>
2022-11-19 11:52:01 -0300

    Configuracao do controle

---
+++ ModeloApi3_1/Solution1/WebApi2/Controllers/v1/ProductController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Features.ProductFeatures.Commands;
+using Application.Features.ProductFeatures.Queries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi2.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    public class ProductController : BaseApiController
+    {
+        /// <summary>
+        /// Creates a New Product.
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ApiConventionMethod(typeof(DefaultApiConventions),
+                     nameof(DefaultApiConventions.Post))]
+        public async Task<IActionResult> Create(CreateProductCommand command)
+        {
+            var entity = await Mediator.Send(command);
+
+
+            return new ObjectResult(entity) { StatusCode = StatusCodes.Status201Created };
+        }
+        /// <summary>
+        /// Gets all Products.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ApiConventionMethod(typeof(DefaultApiConventions),
+                     nameof(DefaultApiConventions.Get))]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await Mediator.Send(new GetAllProductsQuery()));
+        }
+        /// <summary>
+        /// Gets Product Entity by Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        [ApiConventionMethod(typeof(DefaultApiConventions),
+                     nameof(DefaultApiConventions.Get))]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await Mediator.Send(new GetProductByIdQuery { Id = id }));
+        }
+        /// <summary>
+        /// Deletes Product Entity based on Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        [ApiConventionMethod(typeof(DefaultApiConventions),
+                     nameof(DefaultApiConventions.Delete))]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await Mediator.Send(new DeleteProductByIdCommand { Id = id }));
+        }
+        /// <summary>
+        /// Updates the Product Entity based on Id.   
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        [HttpPut("[action]")]
+        [ApiConventionMethod(typeof(DefaultApiConventions),
+                     nameof(DefaultApiConventions.Put))]
+        public async Task<IActionResult> Update(int id, UpdateProductCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+    }
+
+}
